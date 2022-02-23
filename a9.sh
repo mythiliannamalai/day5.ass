5e10ad91bbaaebae6897eac8215d6a220cc9983f
Naveen Kumar Selvaraj <naveenkumars@parkisolutions.com>
2022-02-23 11:06:00 +0530

    qus9

---
+++ a9.sh
@@ -0,0 +1,25 @@
+read -p "enter the week num 1 to 7 =" week
+	if(($week==1))
+	then
+	echo "sunday"
+	elif(($week==2))
+	then
+	echo "monday"
+	elif(($week==3))
+	then
+	echo "tuesday"
+	elif(($week==4))
+	then
+	echo "wednesday"
+	elif(($week==5))
+	then
+	echo "thursday"
+	elif(($week==6))
+	then
+	echo "friday"
+	elif(($week==7))
+	then
+	echo "sater day"
+	else
+	echo "invalid input"
+	fi
