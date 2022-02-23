5f7afdfeae01280fb2dad5f9f07c5f049a336ca7
Naveen Kumar Selvaraj <naveenkumars@parkisolutions.com>
2022-02-23 11:21:12 +0530

    qus14

---
+++ a14.sh
@@ -0,0 +1,14 @@
+read -p "enter the  value : " num
+	case $num in
+	1) echo "unit"
+	;;
+	10) echo "ten"
+	;;
+	100) echo "hundred"
+	;;
+	1000) echo "thousand"
+	;;
+	10000) echo "ten thousand"
+	;;
+	*) echo "invalied input "
+	esac
