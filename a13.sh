7fbb44653a061e9dafa2bf37cdb9a531fd22409f
Naveen Kumar Selvaraj <naveenkumars@parkisolutions.com>
2022-02-23 11:19:03 +0530

    qus13

---
+++ a13.sh
@@ -0,0 +1,18 @@
+read -p "enter the week num : " num
+	case $num in
+	1)echo "sunday"
+	;;
+	2)echo "monday"
+	;;
+	3)echo "tuesday"
+	;;
+	4)echo "wednesday"
+	;;
+	5)echo "thuresday"
+	;;
+	6)echo "friday"
+	;;
+	7)echo "saterday"
+	;;
+	*)echo "invaled input"
+	esac
