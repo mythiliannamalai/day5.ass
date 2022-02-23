e3be10c85cfafe3a0604559833f118f09aa2c3f9
Naveen Kumar Selvaraj <naveenkumars@parkisolutions.com>
2022-02-23 11:03:42 +0530

    qus8

---
+++ a8.sh
@@ -0,0 +1,31 @@
+read -p "enter the 1 to 9  :" num
+	if(($num==1))
+	then
+	echo "one"
+	elif(($num==2))
+	then
+	echo "two"
+	elif(($num==3))
+	then
+	echo "three"
+	elif(($num==4))
+	then
+	echo "four"
+	elif(($num==5))
+	then
+	echo "five"
+	elif(($num==6))
+	then
+	echo "six"
+	elif(($num==7))
+	then
+	echo "seven"
+	elif(($num==8))
+	then
+	echo "eight"
+	elif(($num==9))
+	then
+	echo "nine"
+	else
+	echo "invalied input"
+	fi
